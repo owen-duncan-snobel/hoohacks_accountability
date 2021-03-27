fe505f79be38415432603786050991e574655bb3
hjanday <jandaypreet@gmail.com>
2021-03-27 19:51:09 -0400

    Added card template

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from './components/card';
 {/*import SearchBar from 'material-ui-search-bar';*/}
 
 
@@ -30,7 +31,7 @@ function App() {
 				</a> </nav>
         
         <div className="text-center font-bold">
-					<h1 className="lg:text-9xl text-5xl sm:text-6xl text5xl mb-10 text-blue-600 hover:text-grey duration-300">
+					<h1 className="lg:text-9xl text-5xl sm:text-6xl text5xl mb-10 text-blue-600 hover:text-black duration-300">
 						Track their promises
 					</h1>
 				</div>
@@ -48,7 +49,9 @@ function App() {
 
               </button>
         </div>
+        
     </div>
+    <Card/>
   </div>
 
       </div>
